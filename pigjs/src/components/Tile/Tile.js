6c2d750b9b688d3e6a4da63dc8238ae821b46f50
yokesh <yokeshthirumoorthi@gmail.com>
2021-07-30 13:29:59 -0700

    add zoom effect to images in grid

--- pigjs/src/components/Tile/Tile.js
+++ pigjs/src/components/Tile/Tile.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import { useSpring, animated } from 'react-spring'
-import getImageHeight from '../../utils/getImageHeight'
-import getTileMeasurements from '../../utils/getTileMeasurements'
-import styles from './styles.css'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useSpring, animated } from "react-spring";
+import getImageHeight from "../../utils/getImageHeight";
+import getTileMeasurements from "../../utils/getTileMeasurements";
+import styles from "./styles.css";
 
 const Tile = React.memo(function Tile({
   item,
@@ -15,36 +15,49 @@ const Tile = React.memo(function Tile({
   handleClick,
   windowHeight,
   scrollSpeed,
-  settings,
+  settings
 }) {
+  const isExpanded = activeTileUrl === item.url;
+  const isVideo = item.url.includes(".mp4") || item.url.includes(".mov");
+  const [isFullSizeLoaded, setFullSizeLoaded] = useState(
+    isVideo ? true : false
+  );
 
-  const isExpanded = activeTileUrl === item.url
-  const isVideo = item.url.includes('.mp4') || item.url.includes('.mov')
-  const [isFullSizeLoaded, setFullSizeLoaded] = useState(isVideo ? true : false)
-
-  const { calcWidth, calcHeight, offsetX, offsetY } = getTileMeasurements({ item, windowHeight, settings, containerWidth, containerOffsetTop })
+  const { calcWidth, calcHeight, offsetX, offsetY } = getTileMeasurements({
+    item,
+    windowHeight,
+    settings,
+    containerWidth,
+    containerOffsetTop
+  });
 
   // gridPosition is what has been set by the grid layout logic (in the parent component)
-  const gridPosition = `translate3d(${item.style.translateX}px, ${item.style.translateY}px, 0)`
+  const gridPosition = `translate3d(${item.style.translateX}px, ${item.style.translateY}px, 0)`;
   // screenCenter is positioning logic for when the item is active and expanded
-  const screenCenter = `translate3d(${offsetX}px, ${offsetY}px, 0)`
+  const screenCenter = `translate3d(${offsetX}px, ${offsetY}px, 0)`;
 
   const { width, height, transform, zIndex } = useSpring({
     transform: isExpanded ? screenCenter : gridPosition,
     zIndex: isExpanded ? 10 : 0, // 10 so that it takes a little longer before settling at 0
-    width: isExpanded ? Math.ceil(calcWidth) + 'px' : item.style.width + 'px',
-    height: isExpanded ? Math.ceil(calcHeight) + 'px' : item.style.height + 'px',
+    width: isExpanded ? Math.ceil(calcWidth) + "px" : item.style.width + "px",
+    height: isExpanded
+      ? Math.ceil(calcHeight) + "px"
+      : item.style.height + "px",
     config: { mass: 1.5, tension: 400, friction: 40 }
-  })
+  });
 
   return (
     <animated.a
-      className={`${styles.pigBtn}${isExpanded ? ` ${styles.pigBtnActive}` : ''} pig-btn`}
+      className={`${styles.pigBtn}${
+        isExpanded ? ` ${styles.pigBtnActive}` : ""
+      } pig-btn`}
       // onClick={() => handleClick(item)}
       href={getUrl(item.url, getImageHeight(containerWidth))}
       data-size={`${Math.ceil(calcWidth)}x${Math.ceil(calcHeight)}`}
       style={{
-        outline: isExpanded ? `${settings.gridGap}px solid ${settings.bgColor}` : null,
+        outline: isExpanded
+          ? `${settings.gridGap}px solid ${settings.bgColor}`
+          : null,
         backgroundColor: item.dominantColor,
         zIndex: zIndex.interpolate(t => Math.round(t)),
         width: width.interpolate(t => t),
@@ -52,32 +65,37 @@ const Tile = React.memo(function Tile({
         transform: transform.interpolate(t => t)
       }}
     >
-
-      {useLqip &&
+      {useLqip && (
         // LQIP
         <img
-          className={`${styles.pigImg} ${styles.pigThumbnail}${isFullSizeLoaded ? ` ${styles.pigThumbnailLoaded}` : ''}`}
+          className={`${styles.pigImg} ${styles.pigThumbnail}${
+            isFullSizeLoaded ? ` ${styles.pigThumbnailLoaded}` : ""
+          }`}
           src={getUrl(item.url, settings.thumbnailSize)}
           loading="lazy"
           width={item.style.width}
           height={item.style.height}
           alt=""
         />
-      }
+      )}
 
-      {(scrollSpeed === 'slow') && !isVideo &&
+      {scrollSpeed === "slow" && !isVideo && (
         // grid image
         <img
-          className={`${styles.pigImg} ${styles.pigFull}${isFullSizeLoaded ? ` ${styles.pigFullLoaded}` : ''}`}
+          className={`${styles.pigImg} ${styles.pigFull}${
+            isFullSizeLoaded ? ` ${styles.pigFullLoaded}` : ""
+          }`}
           src={getUrl(item.url, getImageHeight(containerWidth))}
           alt=""
           onLoad={() => setFullSizeLoaded(true)}
         />
-      }
+      )}
 
-      {(scrollSpeed === 'slow') && isVideo &&
+      {scrollSpeed === "slow" && isVideo && (
         <video
-          className={`${styles.pigImg} ${styles.pigThumbnail}${isFullSizeLoaded ? ` ${styles.pigThumbnailLoaded}` : ''}`}
+          className={`${styles.pigImg} ${styles.pigThumbnail}${
+            isFullSizeLoaded ? ` ${styles.pigThumbnailLoaded}` : ""
+          }`}
           src={getUrl(item.url, getImageHeight(containerWidth))}
           onCanPlay={() => setFullSizeLoaded(true)}
           autoPlay
@@ -85,7 +103,7 @@ const Tile = React.memo(function Tile({
           loop
           playsInline
         />
-      }
+      )}
 
       {isExpanded && !isVideo && (
         // full size expanded image
@@ -108,14 +126,14 @@ const Tile = React.memo(function Tile({
         />
       )}
     </animated.a>
-  )
-})
+  );
+});
 
-export default Tile
+export default Tile;
 
 Tile.propTypes = {
   item: PropTypes.object.isRequired,
   containerWidth: PropTypes.number,
   settings: PropTypes.object.isRequired,
-  getUrl: PropTypes.func,
-}
+  getUrl: PropTypes.func
+};
