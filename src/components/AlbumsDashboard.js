6b5d9abb353c5e092c17bff635b95ab1cebbdf65
yokesh <yokeshthirumoorthi@gmail.com>
2021-07-30 04:15:10 -0700

    add nested router

--- src/components/AlbumsDashboard.js
+++ src/components/AlbumsDashboard.js
@@ -6,7 +6,8 @@ import {
   // SaveStatus,
   useCreateAlbum,
 } from "../hooks/server";
-import { Link } from "react-router-dom";
+
+import { Link, useRouteMatch, Switch, Route } from "react-router-dom";
 
 /* This example requires Tailwind CSS v2.0+ */
 import { PlusIcon as PlusIconSolid } from "@heroicons/react/solid";
@@ -30,6 +31,7 @@ import Pig from "pig-react";
 import imageData from "./imageData.json";
 
 import "./base.css";
+import { Navbar } from "react-bootstrap";
 
 function CreateAlbumButton({ onClick }) {
   return (
@@ -256,10 +258,10 @@ function LoginSection() {
 }
 
 const navigation = [
-  { name: "Home", href: "#", icon: HomeIcon, current: false },
-  { name: "Albums", href: "#", icon: CollectionIcon, current: true },
-  { name: "Photos", href: "#", icon: PhotographIcon, current: false },
-  { name: "Logout", href: "#", icon: LogoutIcon, current: false },
+  { name: "Home", href: "", icon: HomeIcon, current: false },
+  { name: "Albums", href: "albums", icon: CollectionIcon, current: true },
+  { name: "Photos", href: "photos", icon: PhotographIcon, current: false },
+  { name: "Logout", href: "login", icon: LogoutIcon, current: false },
 ];
 
 function classNames(...classes) {
@@ -390,152 +392,178 @@ function PigSection({ setMobileMenuOpen }) {
   );
 }
 
-export default function AppPage() {
+function Sidebar({ url }) {
+  return (
+    <div className="hidden w-28 bg-indigo-700 overflow-y-auto md:block">
+      <div className="w-full py-6 flex flex-col items-center">
+        <div className="flex-shrink-0 flex items-center">
+          <img
+            className="h-8 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
+            alt="Workflow"
+          />
+        </div>
+        <div className="flex-1 mt-6 w-full px-2 space-y-1">
+          {navigation.map((item) => (
+            <Link
+              key={item.name}
+              to={`${url}/${item.href}`}
+              className={classNames(
+                item.current
+                  ? "bg-indigo-800 text-white"
+                  : "text-indigo-100 hover:bg-indigo-800 hover:text-white",
+                "group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium"
+              )}
+              aria-current={item.current ? "page" : undefined}
+            >
+              <item.icon
+                className={classNames(
+                  item.current
+                    ? "text-white"
+                    : "text-indigo-300 group-hover:text-white",
+                  "h-6 w-6"
+                )}
+                aria-hidden="true"
+              />
+              <span className="mt-2">{item.name}</span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MobileMenu({ url, mobileMenuOpen, setMobileMenuOpen }) {
+  return (
+    <Transition.Root show={mobileMenuOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        static
+        className="fixed inset-0 z-40 flex md:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="transition-opacity ease-linear duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+        </Transition.Child>
+        <Transition.Child
+          as={Fragment}
+          enter="transition ease-in-out duration-300 transform"
+          enterFrom="-translate-x-full"
+          enterTo="translate-x-0"
+          leave="transition ease-in-out duration-300 transform"
+          leaveFrom="translate-x-0"
+          leaveTo="-translate-x-full"
+        >
+          <div className="relative max-w-xs w-full bg-indigo-700 pt-5 pb-4 flex-1 flex flex-col">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-in-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in-out duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="absolute top-1 right-0 -mr-14 p-1">
+                <button
+                  type="button"
+                  className="h-12 w-12 rounded-full flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-white"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                  <span className="sr-only">Close sidebar</span>
+                </button>
+              </div>
+            </Transition.Child>
+            <div className="flex-shrink-0 px-4 flex items-center">
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
+                alt="Workflow"
+              />
+            </div>
+            <div className="mt-5 flex-1 h-0 px-2 overflow-y-auto">
+              <nav className="h-full flex flex-col">
+                <div className="space-y-1">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      to={`${url}/${item.href}`}
+                      className={classNames(
+                        item.current
+                          ? "bg-indigo-800 text-white"
+                          : "text-indigo-100 hover:bg-indigo-800 hover:text-white",
+                        "group py-2 px-3 rounded-md flex items-center text-sm font-medium"
+                      )}
+                      aria-current={item.current ? "page" : undefined}
+                    >
+                      <item.icon
+                        className={classNames(
+                          item.current
+                            ? "text-white"
+                            : "text-indigo-300 group-hover:text-white",
+                          "mr-3 h-6 w-6"
+                        )}
+                        aria-hidden="true"
+                      />
+                      <span>{item.name}</span>
+                    </Link>
+                  ))}
+                </div>
+              </nav>
+            </div>
+          </div>
+        </Transition.Child>
+        <div className="flex-shrink-0 w-14" aria-hidden="true">
+          {/* Dummy element to force sidebar to shrink to fit close icon */}
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
+
+export default function AppPage({ children }) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  let { path, url } = useRouteMatch();
 
   return (
     <div className="h-screen bg-gray-50 flex overflow-hidden">
       {/* Narrow sidebar */}
-      <div className="hidden w-28 bg-indigo-700 overflow-y-auto md:block">
-        <div className="w-full py-6 flex flex-col items-center">
-          <div className="flex-shrink-0 flex items-center">
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
-              alt="Workflow"
-            />
-          </div>
-          <div className="flex-1 mt-6 w-full px-2 space-y-1">
-            {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className={classNames(
-                  item.current
-                    ? "bg-indigo-800 text-white"
-                    : "text-indigo-100 hover:bg-indigo-800 hover:text-white",
-                  "group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium"
-                )}
-                aria-current={item.current ? "page" : undefined}
-              >
-                <item.icon
-                  className={classNames(
-                    item.current
-                      ? "text-white"
-                      : "text-indigo-300 group-hover:text-white",
-                    "h-6 w-6"
-                  )}
-                  aria-hidden="true"
-                />
-                <span className="mt-2">{item.name}</span>
-              </a>
-            ))}
-          </div>
-        </div>
-      </div>
-
-      {/* Mobile menu */}
-      <Transition.Root show={mobileMenuOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          static
-          className="fixed inset-0 z-40 flex md:hidden"
-          open={mobileMenuOpen}
-          onClose={setMobileMenuOpen}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-          </Transition.Child>
-          <Transition.Child
-            as={Fragment}
-            enter="transition ease-in-out duration-300 transform"
-            enterFrom="-translate-x-full"
-            enterTo="translate-x-0"
-            leave="transition ease-in-out duration-300 transform"
-            leaveFrom="translate-x-0"
-            leaveTo="-translate-x-full"
-          >
-            <div className="relative max-w-xs w-full bg-indigo-700 pt-5 pb-4 flex-1 flex flex-col">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-in-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in-out duration-300"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <div className="absolute top-1 right-0 -mr-14 p-1">
-                  <button
-                    type="button"
-                    className="h-12 w-12 rounded-full flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-white"
-                    onClick={() => setMobileMenuOpen(false)}
-                  >
-                    <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                    <span className="sr-only">Close sidebar</span>
-                  </button>
-                </div>
-              </Transition.Child>
-              <div className="flex-shrink-0 px-4 flex items-center">
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
-                  alt="Workflow"
-                />
-              </div>
-              <div className="mt-5 flex-1 h-0 px-2 overflow-y-auto">
-                <nav className="h-full flex flex-col">
-                  <div className="space-y-1">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-indigo-800 text-white"
-                            : "text-indigo-100 hover:bg-indigo-800 hover:text-white",
-                          "group py-2 px-3 rounded-md flex items-center text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                      >
-                        <item.icon
-                          className={classNames(
-                            item.current
-                              ? "text-white"
-                              : "text-indigo-300 group-hover:text-white",
-                            "mr-3 h-6 w-6"
-                          )}
-                          aria-hidden="true"
-                        />
-                        <span>{item.name}</span>
-                      </a>
-                    ))}
-                  </div>
-                </nav>
-              </div>
-            </div>
-          </Transition.Child>
-          <div className="flex-shrink-0 w-14" aria-hidden="true">
-            {/* Dummy element to force sidebar to shrink to fit close icon */}
-          </div>
-        </Dialog>
-      </Transition.Root>
+      <Sidebar url={url} />
+      <MobileMenu
+        url={url}
+        mobileMenuOpen={mobileMenuOpen}
+        setMobileMenuOpen={setMobileMenuOpen}
+      />
 
       {/* Content area */}
       <div className="flex-1 flex flex-col overflow-auto">
-        {/* Main content */}
-        {/* <MainContentSection setMobileMenuOpen={setMobileMenuOpen} /> */}
-        {/* <MainContentHeader setMobileMenuOpen={setMobileMenuOpen} /> */}
-        {/* <LoginSection /> */}
-        <PigSection setMobileMenuOpen={setMobileMenuOpen} />
+        <Switch>
+          <Route exact path={path}>
+            <MainContentSection setMobileMenuOpen={setMobileMenuOpen} />
+          </Route>
+          <Route path={`${path}/photos`}>
+            <PigSection setMobileMenuOpen={setMobileMenuOpen} />
+          </Route>
+          <Route path={`${path}/albums`}>
+            <MainContentSection setMobileMenuOpen={setMobileMenuOpen} />
+          </Route>
+          <Route path={`${path}/login`}>
+            <MainContentHeader setMobileMenuOpen={setMobileMenuOpen} />
+            <LoginSection />
+          </Route>
+        </Switch>
+        {children}
       </div>
     </div>
   );
--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
-import AlbumsDashboard from "./AlbumsDashboard";
+import AppPageRouter from "./AlbumsDashboard";
 import PhotoGallery from "./PhotoGallery";
 import Upload from "./Upload";
 import Login from "./Login";
@@ -17,7 +17,7 @@ function App() {
     <Router>
       <AuthProvider>
         <Switch>
-          <PrivateRoute exact path="/" component={AlbumsDashboard} />
+          <PrivateRoute exact path="/" component={Login} />
           <PrivateRoute
             exact
             path="/gallery/:album_name"
@@ -26,7 +26,7 @@ function App() {
           <PrivateRoute exact path="/upload/:album_name" component={Upload} />
           <PrivateRoute path="/update-profile" component={UpdateProfile} />
           <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
+          <Route path="/app" component={AppPageRouter} />
           <Route path="/forgot-password" component={ForgotPassword} />
         </Switch>
       </AuthProvider>
