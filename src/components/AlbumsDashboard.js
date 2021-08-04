a31d1b70d6e288aa5207e131d5b39c506ddcb1c7
yokesh <yokeshthirumoorthi@gmail.com>
2021-08-03 20:08:55 -0700

    adjust css in album loading

--- src/components/AlbumsDashboard.js
+++ src/components/AlbumsDashboard.js
@@ -123,10 +123,22 @@ function TableComponent({ getAlbumsStatus, albums, setSelectedAlbum }) {
       );
     default:
       return (
-        <>
-          <ClockIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
-          <span className="ml-3">Loading Please Wait</span>
-        </>
+        <section className="text-gray-600 body-font">
+          <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+            <div className="text-center lg:w-2/3 w-full">
+              <p className="mb-8 leading-relaxed">
+                You may need to select an album to view photos
+              </p>
+              <div className="flex justify-center">
+                <ClockIcon
+                  className="w-5 h-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </div>
+              <span className="ml-3">Loading Please Wait</span>
+            </div>
+          </div>
+        </section>
       );
   }
 }
